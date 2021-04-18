c16465ae0fe3c39be3e9596cdb55bda9bc1dbb0c
kerberos <kerberos>
2021-04-18 13:43:14 -0500

    working start script

--- kerberos.py
+++ kerberos.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import os
+import re
+import socket
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
@@ -8,13 +10,15 @@ import json
 import time
 
 SERVERIP = '192.168.0.10'
-FREQ = 121.65
 
+print("Make sure that either the radio band of interest is completely silent (no beacons or radio station broadcasting), or all kerberos antenna are disconnected from the board, in order to perform sync process correctly")
+input("Press Enter to continue...")
+print("Going through kerberos settings and syncing sequence, will take about 30 secs. Be patient...")
 
 options = Options()
 options.headless = True
 driver = webdriver.Chrome(options=options)
-driver.implicitly_wait(10)
+driver.implicitly_wait(30)
 
 """ Init page: disable DC compensation and FIR filter, and start processing """
 driver.get("http://" + SERVERIP + ":8080/init")
@@ -37,8 +41,10 @@ start_btn.click()
 
 """ Sync page: enable noise source and do sample sync and IQ calibration, then disable noise source """
 driver.get("http://" + SERVERIP + ":8080/sync")
+
 # check_sync_btn = driver.find_element_by_css_selector("input[value='Enable Noise Source & Sync Display']")
 # check_sync_btn.click()
+
 sync  = driver.find_element_by_css_selector("input[name='en_sync']")
 if (not sync.is_selected()):
     sync.click()
@@ -77,34 +83,45 @@ update = driver.find_element_by_css_selector("input[value='Update']")
 update.click()
 
 """ Init page: enable DC comp and filter again """
-# driver.get("http://" + SERVERIP + ":8080/init")
+driver.get("http://" + SERVERIP + ":8080/init")
 
-# dc_comp = driver.find_element_by_css_selector("input[name='dc_comp']")
-# if (not dc_comp.is_selected()):
-#     dc_comp.click()
+dc_comp = driver.find_element_by_css_selector("input[name='dc_comp']")
+if (not dc_comp.is_selected()):
+    dc_comp.click()
 
-# fir_size = driver.find_element_by_css_selector("input[name='fir_size']")
-# fir_size.clear()
-# fir_size.send_keys('100')
+fir_size = driver.find_element_by_css_selector("input[name='fir_size']")
+fir_size.clear()
+fir_size.send_keys('100')
 
-# iq_set_btn = driver.find_element_by_css_selector("input[value='Update IQ Paramaters']")
-# iq_set_btn.click()
+iq_set_btn = driver.find_element_by_css_selector("input[value='Update IQ Paramaters']")
+iq_set_btn.click()
 
 """ DOA page: enable DOA """
-# driver.get("http://" + SERVERIP + ":8080/doa")
+driver.get("http://" + SERVERIP + ":8080/doa")
 
-# doa = driver.find_element_by_css_selector("input[name='en_doa']")
-# if (not doa.is_selected()):
-#     doa.click()
+doa = driver.find_element_by_css_selector("input[name='en_doa']")
+if (not doa.is_selected()):
+    doa.click()
 
-# update = driver.find_element_by_css_selector("input[value='Update DOA']")
-# update.click()
+update = driver.find_element_by_css_selector("input[value='Update DOA']")
+update.click()
+
+""" Compass page: get the bearing info """
+driver.get("http://192.168.0.10:8081/compass.html?MIN_PWR=10&MIN_CONF=3")
+doa = driver.find_element_by_id("doa")
 
 
-# driver.get("http://192.168.0.10:8081/compass.html")
-# WebDriverWait(driver, 1000)
-# doa = driver.find_elements_by_id("doa")
-# while(True):
-#     print(doa[0].text)
-#     time.sleep(1)
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+print("Sending bearing value through UDP...")
+print("UDP target IP: %s" % UDP_IP)
+print("UDP target port: %s" % UDP_PORT)
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+
+while(True):
+    bearing = re.findall(r'\d+', doa.text)[0]
+    print("Bearing:{}".format(bearing))
+    sock.sendto(bearing.encode(), (UDP_IP, UDP_PORT))
+    time.sleep(1)
 
