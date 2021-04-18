0cf29e0eea10584f5a070c5f2883c232c1db13fd
kerberos <kerberos>
2021-04-18 13:43:27 -0500

    working start script

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp settings.json.immutable $HOME/Desktop/kerberossdr/settings.json
+sudo Xvfb :10 -ac -screen 0 800x600x8 &
+cd $HOME/Desktop/kerberossdr && DISPLAY=:10 ./run.sh
+while ! pgrep -f hydra > /dev/null 2>&1 ; do sleep 1; done; echo "wait a few sec for Hydra to start..."; sleep 5
+cd $HOME/Desktop/kerberos_scripts && DISPLAY=:10 ./kerberos.py
+
